5cc567f1b91f3249ca4bf77ebbf7fe35cad2226b
Christopher Reinken <christopherreinken@gmail.com>
2020-03-27 15:19:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(num)
   results = []
-  [-num..num].step(1).to_a.each do |test_num|
+  (-num..num).step(1).to_a.each do |test_num|
     if num % test_num == 0
       results.push test_num
     end
