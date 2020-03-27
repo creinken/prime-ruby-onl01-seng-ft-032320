7835ed1f2bc8b81b58e6e817b3c13b4e227d0900
Christopher Reinken <christopherreinken@gmail.com>
2020-03-27 16:45:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,9 @@ def prime?(num)
   prime = false
   (-num..num).step(1).to_a.each do |test_num|
     if test_num != 0 && num % test_num == 0 && !([1,num].include?(test_num))
-      puts num
       results.push test_num
     end
     results.length == 0 ? prime = true : prime = false
   end
-  puts "#{results.length} results"
-  puts prime
   prime
 end
-
-prime?(gets)
